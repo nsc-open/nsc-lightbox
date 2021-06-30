58de0c4ac958d411b070a142b0113c1c4ad4bf25
aaxuyun <784266498@qq.com>
2021-06-30 11:03:33 +0800

    udpate

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,66 @@
-import pkg from './package.json';
-import createModuleConfig from './config/rollupRenderModuleConfig';
-import createStyleConfig from './config/rollupRenderStyleConfig';
-import cModuleMap from './config/obtainComponentsName';
+// import pkg from './package.json';
+// import createModuleConfig from './config/rollupRenderModuleConfig';
+// import createStyleConfig from './config/rollupRenderStyleConfig';
+// import cModuleMap from './config/obtainComponentsName';
 
-const external = Object.keys(pkg.dependencies);
-const isDev = false
+// const external = Object.keys(pkg.dependencies);
+// const isDev = false
 
-/*
-    dev 情况下不做样式抽离
-    其他环境下，除了基本的 js 打包外，遍历要拆分的模块，分别生成一个配置项，在这个配置项中处理各自的样式分离
-*/
-const rollupConfig = isDev
-  ? createModuleConfig(external, isDev)
-  : [
-    createModuleConfig(external, isDev)
-  ].concat(
-    Object.keys(cModuleMap).map(moduleName => createStyleConfig(moduleName, external))
-  );
+// /*
+//     dev 情况下不做样式抽离
+//     其他环境下，除了基本的 js 打包外，遍历要拆分的模块，分别生成一个配置项，在这个配置项中处理各自的样式分离
+// */
+// const rollupConfig = isDev
+//   ? createModuleConfig(external, isDev)
+//   : [
+//     createModuleConfig(external, isDev)
+//   ].concat(
+//     Object.keys(cModuleMap).map(moduleName => createStyleConfig(moduleName, external))
+//   );
 
-export default rollupConfig
+// export default rollupConfig
+
+import babel from '@rollup/plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+import svgr from '@svgr/rollup'
+import pkg from './package.json'
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    postcss({
+      extensions: ['.less', '.css'],
+      use: [
+        ['less', {
+          javascriptEnabled: true
+        }]
+      ],
+    }),
+    url(),
+    svgr(),
+    babel({
+       exclude: 'node_modules/**',
+       babelHelpers: 'bundled'
+    }),
+    resolve(),
+    commonjs({
+      namedExports: { 'react-is': ['isMemo'] },
+    })
+  ]
+}
